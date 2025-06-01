9b1480a0d2888c174b4a0aba2adb3b6c3d679cfe
Xavier D'Mello <xaviermdmello@gmail.com>
2025-06-01 03:49:52 +0200

    a

--- frontend/app/config.tsx
+++ frontend/app/config.tsx
@@ -1,4 +1,4 @@
-import { flowTestnet, hederaTestnet } from "wagmi/chains";
+import { flowMainnet, flowTestnet, hederaTestnet, hedera, rootstockTestnet, rootstock } from "wagmi/chains";
 
 type PayMePrettyPleaseConfig = {
   [chainId: number]: {
@@ -8,9 +8,12 @@ type PayMePrettyPleaseConfig = {
 
 const config: PayMePrettyPleaseConfig = {
   [hederaTestnet.id]: {
-    address: "0x6aaefea0249b7a04af44a3e7810aef7f82822dab",
+    address: "0x18b27c5b9655a0189184f1bd1c02b174bf32d81e",
   },
   [flowTestnet.id]: {
+    address: "0x8ef6655d01E8A51bE8C3De998d7729c0278f4d06",
+  },
+  [flowMainnet.id]: {
     address: "0x8ef6655d01E8A51bE8C3De998d7729c0278f4d06",
   },
 };
