efa1c14a7ae95e266862e289737e027616ebee5c
Xavier D'Mello <xaviermdmello@gmail.com>
2025-06-01 04:18:11 +0200

    reee

--- frontend/wagmi.ts
+++ frontend/wagmi.ts
@@ -1,9 +1,29 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { hederaTestnet, flowTestnet} from "wagmi/chains";
+import {
+  flowMainnet,
+  flowTestnet,
+  hederaTestnet,
+  hedera,
+  rootstockTestnet,
+  rootstock,
+  flareTestnet,
+} from "wagmi/chains";
 
 export const config = getDefaultConfig({
   appName: "PayMePrettyPlease",
   projectId: "2d35b7e036ea0ce5bac969cca666cad5",
-  chains: [hederaTestnet, flowTestnet],
+  chains: [
+    // Hedera networks
+    hederaTestnet,
+
+    // Flow networks
+    flowTestnet,
+    flowMainnet,
+    // Rootstock networks
+    rootstockTestnet,
+
+    // Flare networks
+    flareTestnet,
+  ],
   ssr: true,
 });
